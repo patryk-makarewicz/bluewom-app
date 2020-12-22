c5c4483a3169ce90545d7ad3cb158566b1e11262
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2020-12-22 21:00:10 +0100

    build: clean project and new start with it

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 import GlobalStyle from 'theme/GlobalStyle';
-import Rates from 'views/Rates';
-import { Provider } from 'react-redux';
-import store from 'store';
 
 const MyHeader = styled.header`
   font-size: calc(16px + 2vmin);
@@ -11,13 +8,10 @@ const MyHeader = styled.header`
 `;
 
 const Root = () => (
-  <Provider store={store}>
-    <div>
-      <GlobalStyle />
-      <MyHeader>Add to favorite</MyHeader>
-      <Rates />
-    </div>
-  </Provider>
+  <div>
+    <GlobalStyle />
+    <MyHeader>Add to favorite</MyHeader>
+  </div>
 );
 
 export default Root;
