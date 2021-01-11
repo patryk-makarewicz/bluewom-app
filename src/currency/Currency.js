0a7e0dece5eb8f8876db4d2dfcf11fc88c036599
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2021-01-11 14:48:32 +0100

    feat: add useEffect to fetch data on start without button

--- src/currency/Currency.js
+++ src/currency/Currency.js
@@ -1,33 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchCurrencies } from 'reducers';
 
 import CurrencyList from './components/CurrencyList';
-
-const Currency = (props) => {
-  const { currencies, isLoading } = props;
-  const fetchData = () => {
-    props.fetchCurrencies();
-  };
-  return (
-    <div>
-      <h1>Chose Your favorite currencies</h1>
-      <div>
-        {isLoading && <p>Loading...</p>}
-        <button type="button" onClick={fetchData}>
-          Fetch
-        </button>
-        <CurrencyList currencies={currencies} />
-      </div>
-    </div>
-  );
-};
 
 const mapStateToProps = (state) => ({
   currencies: state.currencies,
   isLoading: state.isLoading,
   isError: state.isError,
 });
+
+const Currency = (props) => {
+  const { currencies, isLoading } = props;
+
+  useEffect(() => {
+    props.fetchCurrencies();
+  }, []);
+
+  return (
+    <div>
+      <h1>Chose Your favorite currencies</h1>
+      <div>
+        {isLoading && <p>Loading...</p>}
+        <CurrencyList currencies={currencies} />
+      </div>
+    </div>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
   fetchCurrencies: () => dispatch(fetchCurrencies()),
