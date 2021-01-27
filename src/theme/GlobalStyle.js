c5afec63da9eb3f5e2df78a91ae931308ed9f3fd
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2021-01-27 16:12:43 +0100

    feat: startview styles ready

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -16,14 +16,8 @@ const GlobalStyle = createGlobalStyle`
   body {
     font-size: 1.6rem;
     font-family: 'Roboto', sans-serif;;
-    background-color: #282c34;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    color: white;
+    background-color: white;
+    color: black;
     margin: 0;
     padding: 0;
   }
--- src/views/StartView.js
+++ src/views/StartView.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
+import Button from '@material-ui/core/Button';
+import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+import styles from './StartView.module.scss';
+
 const StartView = () => (
-  <header>
-    <h1>CURRENCY APP</h1>
-    <p>This is an app for checking the value of your favorite currencies from Bank NBP.</p>
-  </header>
+  <div className={styles.wrapper}>
+    <div className={styles.inner__wrapper}>
+      <MonetizationOnIcon style={{ fontSize: 250 }} color="primary" />
+      <header className={styles.header}>
+        <h1 className={styles.header__title}>CURRENCY APP</h1>
+        <p className={styles.header__paragraph}>
+          This is an app for checking the value of your favorite currencies from Bank NBP.
+        </p>
+        <Button variant="outlined" color="primary">
+          <span className={styles.header__button}>GET STARTED</span>
+        </Button>
+      </header>
+    </div>
+  </div>
 );
 
 export default StartView;
