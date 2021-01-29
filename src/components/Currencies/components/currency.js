a7157ca86e342bd08ba9d0b98590f77d31606454
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2021-01-29 23:31:45 +0100

    add animation and condition to render

--- src/components/Currencies/components/currency.js
+++ src/components/Currencies/components/currency.js
@@ -22,28 +22,47 @@ const Currency = ({ currency, addToFavorites }) => {
   };
 
   return (
-    <div className="wrapper" style={styles.wrapper}>
-      <div className="currency">
-        <div className="currency__name">{currency.currency}</div>
-        <div className="currency__value">
-          <div>{currency.code}</div>
-          <div>{currency.mid.toFixed(4)} PLN</div>
+    <>
+      {clicked === false ? (
+        <div className="wrapper" style={styles.wrapper}>
+          <div className="currency">
+            <div className="currency__name">{currency.currency}</div>
+            <div className="currency__value">
+              <div>{currency.code}</div>
+              <div>{currency.mid.toFixed(4)} PLN</div>
+            </div>
+          </div>
+          <div className="currency__star">
+            <button
+              className="currency__button"
+              type="button"
+              onClick={() => addToFavorites(currency.code)}
+            >
+              <StarBorderIcon style={{ fontSize: 30, color: '#939597' }} onClick={handleClicked} />
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="currency__star">
-        <button
-          className="currency__button"
-          type="button"
-          onClick={() => addToFavorites(currency.code)}
-        >
-          {clicked === false ? (
-            <StarBorderIcon style={{ fontSize: 30, color: '#939597' }} onClick={handleClicked} />
-          ) : (
-            <StarIcon style={{ fontSize: 30, color: '#fbc02d' }} onClick={handleClicked} />
-          )}
-        </button>
-      </div>
-    </div>
+      ) : (
+        <div className="wrapper" style={styles.wrapper}>
+          <div className="currency">
+            <div className="currency__value currency__value--fav">
+              <div>
+                <span className="blue">{currency.code}</span> IS IN FAVORITES
+              </div>
+            </div>
+          </div>
+          <div className="currency__star">
+            <button
+              className="currency__button"
+              type="button"
+              onClick={() => addToFavorites(currency.code)}
+            >
+              <StarIcon style={{ fontSize: 30, color: '#fbc02d' }} onClick={handleClicked} />
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
