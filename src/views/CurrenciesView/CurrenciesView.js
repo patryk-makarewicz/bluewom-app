c6c2ef52aab75be9f1892c10014fa2707c404acb
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2021-01-29 21:58:01 +0100

    feat: add counter/link of fav items

--- src/views/CurrenciesView/CurrenciesView.js
+++ src/views/CurrenciesView/CurrenciesView.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
 
+import Badge from '@material-ui/core/Badge';
+import StarIcon from '@material-ui/icons/Star';
 import Currencies from '../../components/Currencies/currencies';
 import styles from './Currencies.module.scss';
 
-const CurrenciesView = () => (
+const CurrenciesView = ({ favCart }) => (
   <div className={styles.wrapper}>
     <p className={styles.currencies}>Add your currencies to favorites and check their value.</p>
+    <p className={styles.currencies}>
+      You have in favorites
+      <NavLink className={styles.currencies__link} to="/favorites">
+        <Badge badgeContent={favCart.length} color="secondary" showZero>
+          <StarIcon style={{ fontSize: 40, color: '#fbc02d', marginBottom: '5px' }} />
+        </Badge>
+      </NavLink>
+    </p>
+
     <Currencies />
   </div>
 );
 
-export default CurrenciesView;
+const mapStateToProps = (state) => ({
+  favCart: state.currencies.favCart,
+});
+
+export default connect(mapStateToProps)(CurrenciesView);
